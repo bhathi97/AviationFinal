40366091c6fcf36a1d2e1f2458e0d4111a9e2f34
bhathi97 <bhathiya.rb@gmail.com>
2023-03-17 14:15:09 +0530

    randi2

--- UserControlAF.Designer.vb
+++ UserControlAF.Designer.vb
@@ -22,7 +22,12 @@ Partial Class UserControlAF
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As DataGridViewCellStyle = New DataGridViewCellStyle()
         Panel1 = New Panel()
+        btnDelete = New FontAwesome.Sharp.IconButton()
+        btnUpdate = New FontAwesome.Sharp.IconButton()
         btnSaveFlight = New FontAwesome.Sharp.IconButton()
         dpDate = New DateTimePicker()
         Label9 = New Label()
@@ -41,12 +46,24 @@ Partial Class UserControlAF
         Label2 = New Label()
         Label1 = New Label()
         Panel2 = New Panel()
+        dgvFlight = New DataGridView()
+        fid1 = New DataGridViewTextBoxColumn()
+        fno = New DataGridViewTextBoxColumn()
+        alc = New DataGridViewTextBoxColumn()
+        std1 = New DataGridViewTextBoxColumn()
+        dipart = New DataGridViewTextBoxColumn()
+        eta = New DataGridViewTextBoxColumn()
+        dt = New DataGridViewTextBoxColumn()
         Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        CType(dgvFlight, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Panel1
         ' 
         Panel1.BackColor = SystemColors.ControlLight
+        Panel1.Controls.Add(btnDelete)
+        Panel1.Controls.Add(btnUpdate)
         Panel1.Controls.Add(btnSaveFlight)
         Panel1.Controls.Add(dpDate)
         Panel1.Controls.Add(Label9)
@@ -65,10 +82,59 @@ Partial Class UserControlAF
         Panel1.Controls.Add(Label2)
         Panel1.Controls.Add(Label1)
         Panel1.Dock = DockStyle.Left
+        Panel1.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
         Panel1.Location = New Point(0, 0)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(478, 475)
+        Panel1.Size = New Size(478, 531)
         Panel1.TabIndex = 0
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.BackColor = Color.FromArgb(CByte(0), CByte(40), CByte(77))
+        btnDelete.Cursor = Cursors.Hand
+        btnDelete.FlatAppearance.BorderColor = SystemColors.ButtonFace
+        btnDelete.FlatAppearance.BorderSize = 0
+        btnDelete.FlatAppearance.MouseDownBackColor = Color.White
+        btnDelete.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        btnDelete.FlatStyle = FlatStyle.Flat
+        btnDelete.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
+        btnDelete.ForeColor = Color.White
+        btnDelete.IconChar = FontAwesome.Sharp.IconChar.TrashAlt
+        btnDelete.IconColor = Color.White
+        btnDelete.IconFont = FontAwesome.Sharp.IconFont.Solid
+        btnDelete.IconSize = 20
+        btnDelete.Location = New Point(289, 446)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(150, 35)
+        btnDelete.TabIndex = 19
+        btnDelete.Text = "DELETE"
+        btnDelete.TextAlign = ContentAlignment.MiddleRight
+        btnDelete.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnDelete.UseVisualStyleBackColor = False
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.BackColor = Color.FromArgb(CByte(0), CByte(40), CByte(77))
+        btnUpdate.Cursor = Cursors.Hand
+        btnUpdate.FlatAppearance.BorderColor = SystemColors.ButtonFace
+        btnUpdate.FlatAppearance.BorderSize = 0
+        btnUpdate.FlatAppearance.MouseDownBackColor = Color.White
+        btnUpdate.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(0), CByte(60), CByte(179))
+        btnUpdate.FlatStyle = FlatStyle.Flat
+        btnUpdate.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
+        btnUpdate.ForeColor = Color.White
+        btnUpdate.IconChar = FontAwesome.Sharp.IconChar.PenToSquare
+        btnUpdate.IconColor = Color.White
+        btnUpdate.IconFont = FontAwesome.Sharp.IconFont.Solid
+        btnUpdate.IconSize = 20
+        btnUpdate.Location = New Point(289, 401)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(150, 35)
+        btnUpdate.TabIndex = 18
+        btnUpdate.Text = "UPDATE"
+        btnUpdate.TextAlign = ContentAlignment.MiddleRight
+        btnUpdate.TextImageRelation = TextImageRelation.ImageBeforeText
+        btnUpdate.UseVisualStyleBackColor = False
         ' 
         ' btnSaveFlight
         ' 
@@ -79,13 +145,13 @@ Partial Class UserControlAF
         btnSaveFlight.FlatAppearance.MouseDownBackColor = Color.White
         btnSaveFlight.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(0), CByte(60), CByte(179))
         btnSaveFlight.FlatStyle = FlatStyle.Flat
-        btnSaveFlight.Font = New Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        btnSaveFlight.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
         btnSaveFlight.ForeColor = Color.White
         btnSaveFlight.IconChar = FontAwesome.Sharp.IconChar.FloppyDisk
         btnSaveFlight.IconColor = Color.White
         btnSaveFlight.IconFont = FontAwesome.Sharp.IconFont.Solid
         btnSaveFlight.IconSize = 20
-        btnSaveFlight.Location = New Point(289, 339)
+        btnSaveFlight.Location = New Point(289, 357)
         btnSaveFlight.Name = "btnSaveFlight"
         btnSaveFlight.Size = New Size(150, 35)
         btnSaveFlight.TabIndex = 17
@@ -97,7 +163,7 @@ Partial Class UserControlAF
         ' dpDate
         ' 
         dpDate.Format = DateTimePickerFormat.Short
-        dpDate.Location = New Point(235, 267)
+        dpDate.Location = New Point(235, 285)
         dpDate.Name = "dpDate"
         dpDate.Size = New Size(204, 23)
         dpDate.TabIndex = 16
@@ -106,7 +172,7 @@ Partial Class UserControlAF
         ' Label9
         ' 
         Label9.AutoSize = True
-        Label9.Location = New Point(99, 273)
+        Label9.Location = New Point(99, 291)
         Label9.Name = "Label9"
         Label9.Size = New Size(34, 15)
         Label9.TabIndex = 15
@@ -114,7 +180,7 @@ Partial Class UserControlAF
         ' cbDT
         ' 
         cbDT.FormattingEnabled = True
-        cbDT.Location = New Point(235, 223)
+        cbDT.Location = New Point(235, 241)
         cbDT.Name = "cbDT"
         cbDT.Size = New Size(204, 23)
         cbDT.TabIndex = 14
@@ -122,7 +188,7 @@ Partial Class UserControlAF
         ' Label8
         ' 
         Label8.AutoSize = True
-        Label8.Location = New Point(98, 228)
+        Label8.Location = New Point(98, 246)
         Label8.Name = "Label8"
         Label8.Size = New Size(97, 15)
         Label8.TabIndex = 13
@@ -132,7 +198,7 @@ Partial Class UserControlAF
         Label6.BackColor = SystemColors.Window
         Label6.BorderStyle = BorderStyle.FixedSingle
         Label6.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
-        Label6.Location = New Point(389, 182)
+        Label6.Location = New Point(389, 200)
         Label6.Name = "Label6"
         Label6.Size = New Size(50, 23)
         Label6.TabIndex = 12
@@ -143,7 +209,7 @@ Partial Class UserControlAF
         ' 
         Label7.AutoSize = True
         Label7.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        Label7.Location = New Point(369, 181)
+        Label7.Location = New Point(369, 199)
         Label7.Name = "Label7"
         Label7.Size = New Size(13, 20)
         Label7.TabIndex = 11
@@ -154,7 +220,7 @@ Partial Class UserControlAF
         ' 
         Label5.AutoSize = True
         Label5.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        Label5.Location = New Point(291, 182)
+        Label5.Location = New Point(291, 200)
         Label5.Name = "Label5"
         Label5.Size = New Size(13, 20)
         Label5.TabIndex = 9
@@ -164,7 +230,7 @@ Partial Class UserControlAF
         ' cbStd1
         ' 
         cbStd1.FormattingEnabled = True
-        cbStd1.Location = New Point(311, 182)
+        cbStd1.Location = New Point(311, 200)
         cbStd1.Name = "cbStd1"
         cbStd1.Size = New Size(50, 23)
         cbStd1.TabIndex = 8
@@ -172,7 +238,7 @@ Partial Class UserControlAF
         ' cbStd
         ' 
         cbStd.FormattingEnabled = True
-        cbStd.Location = New Point(235, 182)
+        cbStd.Location = New Point(235, 200)
         cbStd.Name = "cbStd"
         cbStd.Size = New Size(50, 23)
         cbStd.TabIndex = 7
@@ -180,7 +246,7 @@ Partial Class UserControlAF
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(99, 187)
+        Label4.Location = New Point(99, 205)
         Label4.Name = "Label4"
         Label4.Size = New Size(96, 15)
         Label4.TabIndex = 6
@@ -189,7 +255,7 @@ Partial Class UserControlAF
         ' 
         lblAName.BackColor = SystemColors.Window
         lblAName.BorderStyle = BorderStyle.FixedSingle
-        lblAName.Location = New Point(235, 141)
+        lblAName.Location = New Point(235, 159)
         lblAName.Name = "lblAName"
         lblAName.Size = New Size(204, 23)
         lblAName.TabIndex = 5
@@ -197,14 +263,14 @@ Partial Class UserControlAF
         ' cbairlinecategory
         ' 
         cbairlinecategory.FormattingEnabled = True
-        cbairlinecategory.Location = New Point(235, 109)
+        cbairlinecategory.Location = New Point(235, 127)
         cbairlinecategory.Name = "cbairlinecategory"
         cbairlinecategory.Size = New Size(204, 23)
         cbairlinecategory.TabIndex = 4
         ' 
         ' tbflightNo
         ' 
-        tbflightNo.Location = New Point(235, 65)
+        tbflightNo.Location = New Point(235, 83)
         tbflightNo.Name = "tbflightNo"
         tbflightNo.Size = New Size(204, 23)
         tbflightNo.TabIndex = 3
@@ -212,7 +278,7 @@ Partial Class UserControlAF
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(98, 114)
+        Label3.Location = New Point(98, 132)
         Label3.Name = "Label3"
         Label3.Size = New Size(110, 15)
         Label3.TabIndex = 2
@@ -220,7 +286,8 @@ Partial Class UserControlAF
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(99, 69)
+        Label2.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(99, 87)
         Label2.Name = "Label2"
         Label2.Size = New Size(66, 15)
         Label2.TabIndex = 1
@@ -229,7 +296,7 @@ Partial Class UserControlAF
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label1.Location = New Point(68, 15)
+        Label1.Location = New Point(68, 24)
         Label1.Name = "Label1"
         Label1.Size = New Size(183, 24)
         Label1.TabIndex = 0
@@ -237,11 +304,104 @@ Partial Class UserControlAF
         ' Panel2
         ' 
         Panel2.BackColor = SystemColors.ActiveCaption
+        Panel2.Controls.Add(dgvFlight)
         Panel2.Dock = DockStyle.Fill
         Panel2.Location = New Point(478, 0)
         Panel2.Name = "Panel2"
-        Panel2.Size = New Size(276, 475)
+        Panel2.Padding = New Padding(10)
+        Panel2.Size = New Size(683, 531)
         Panel2.TabIndex = 1
+        ' 
+        ' dgvFlight
+        ' 
+        dgvFlight.AllowUserToAddRows = False
+        dgvFlight.AllowUserToDeleteRows = False
+        dgvFlight.BackgroundColor = SystemColors.ActiveCaption
+        dgvFlight.BorderStyle = BorderStyle.None
+        DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = SystemColors.Control
+        DataGridViewCellStyle1.Font = New Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        DataGridViewCellStyle1.ForeColor = SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
+        dgvFlight.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        dgvFlight.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvFlight.Columns.AddRange(New DataGridViewColumn() {fid1, fno, alc, std1, dipart, eta, dt})
+        DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle2.BackColor = SystemColors.Window
+        DataGridViewCellStyle2.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
+        DataGridViewCellStyle2.ForeColor = SystemColors.ControlText
+        DataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = DataGridViewTriState.False
+        dgvFlight.DefaultCellStyle = DataGridViewCellStyle2
+        dgvFlight.Dock = DockStyle.Fill
+        dgvFlight.Location = New Point(10, 10)
+        dgvFlight.Name = "dgvFlight"
+        dgvFlight.ReadOnly = True
+        DataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle3.BackColor = SystemColors.Control
+        DataGridViewCellStyle3.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
+        DataGridViewCellStyle3.ForeColor = SystemColors.WindowText
+        DataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = DataGridViewTriState.True
+        dgvFlight.RowHeadersDefaultCellStyle = DataGridViewCellStyle3
+        dgvFlight.RowHeadersWidth = 4
+        dgvFlight.RowTemplate.Height = 25
+        dgvFlight.RowTemplate.ReadOnly = True
+        dgvFlight.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        dgvFlight.Size = New Size(663, 511)
+        dgvFlight.TabIndex = 0
+        ' 
+        ' fid1
+        ' 
+        fid1.HeaderText = "FID"
+        fid1.Name = "fid1"
+        fid1.ReadOnly = True
+        fid1.Width = 50
+        ' 
+        ' fno
+        ' 
+        fno.HeaderText = "FLIGHT NO"
+        fno.Name = "fno"
+        fno.ReadOnly = True
+        ' 
+        ' alc
+        ' 
+        alc.HeaderText = "AIRLINE CATEGORY"
+        alc.Name = "alc"
+        alc.ReadOnly = True
+        alc.Width = 150
+        ' 
+        ' std1
+        ' 
+        std1.HeaderText = "STD"
+        std1.Name = "std1"
+        std1.ReadOnly = True
+        std1.Width = 75
+        ' 
+        ' dipart
+        ' 
+        dipart.HeaderText = "DEPARTURE TIME"
+        dipart.Name = "dipart"
+        dipart.ReadOnly = True
+        dipart.Width = 150
+        ' 
+        ' eta
+        ' 
+        eta.HeaderText = "ETA"
+        eta.Name = "eta"
+        eta.ReadOnly = True
+        eta.Width = 125
+        ' 
+        ' dt
+        ' 
+        dt.HeaderText = "DATE"
+        dt.Name = "dt"
+        dt.ReadOnly = True
+        dt.Width = 150
         ' 
         ' UserControlAF
         ' 
@@ -251,9 +411,11 @@ Partial Class UserControlAF
         Controls.Add(Panel2)
         Controls.Add(Panel1)
         Name = "UserControlAF"
-        Size = New Size(754, 475)
+        Size = New Size(1161, 531)
         Panel1.ResumeLayout(False)
         Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        CType(dgvFlight, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
@@ -276,4 +438,14 @@ Partial Class UserControlAF
     Friend WithEvents dpDate As DateTimePicker
     Friend WithEvents Label9 As Label
     Friend WithEvents btnSaveFlight As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnUpdate As FontAwesome.Sharp.IconButton
+    Friend WithEvents btnDelete As FontAwesome.Sharp.IconButton
+    Friend WithEvents dgvFlight As DataGridView
+    Friend WithEvents fid1 As DataGridViewTextBoxColumn
+    Friend WithEvents fno As DataGridViewTextBoxColumn
+    Friend WithEvents alc As DataGridViewTextBoxColumn
+    Friend WithEvents std1 As DataGridViewTextBoxColumn
+    Friend WithEvents dipart As DataGridViewTextBoxColumn
+    Friend WithEvents eta As DataGridViewTextBoxColumn
+    Friend WithEvents dt As DataGridViewTextBoxColumn
 End Class
