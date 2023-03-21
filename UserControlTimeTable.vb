5283f53c318d38f219bc31e9d374fbbed47a76ab
bhathi97 <bhathiya.rb@gmail.com>
2023-03-21 23:48:36 +0530

    2023.3.21 lastpush

--- UserControlTimeTable.vb
+++ UserControlTimeTable.vb
@@ -221,35 +221,11 @@ Public Class UserControlTimeTable
 
     End Sub
 
+
     'Handles ContextMenuStrip1.ItemClicked
     Private Sub ContextMenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles ContextMenuStrip1.ItemClicked
         Try
-            Select Case e.ClickedItem.Name
-
-                Case "AddRow"
-                    Dim dv As DataView = CType(dgvMain.DataSource, DataView)
-                    Dim newRow As DataRowView = dv.AddNew()
-                    Dim selectedIndex As Integer = dgvMain.SelectedRows(0).Index ' Get the index of the selected row
-                    dv.Table.Rows.InsertAt(newRow.Row, selectedIndex + 1) ' Insert the new row below the selected row
-                    newRow.EndEdit()
-
-                    ' Update the NO column values
-                    For i As Integer = 0 To dgvMain.Rows.Count - 1
-                        dgvMain.Rows(i).Cells("noo").Value = i + 1
-                    Next
-
-                Case "DeleteRow"
-                    Dim dv As DataView = CType(dgvMain.DataSource, DataView)
-                    For Each row As DataGridViewRow In dgvMain.SelectedRows
-                        dv.Delete(row.Index) ' Remove the selected rows from the data source
-                    Next
-
-                    ' Update the NO column values
-                    For i As Integer = 0 To dgvMain.Rows.Count - 1
-                        dgvMain.Rows(i).Cells("noo").Value = i + 1
-                    Next
-            End Select
-            dgvMain.Refresh()
+            TTGridRowSettingModule.clickEvent(e, dgvMain)
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
