da14ee605d1faad710735d4828567667bee47e8e
bhathi97 <bhathiya.rb@gmail.com>
2023-04-27 21:50:39 +0530

    2023.4.27 night1

--- DatabaseConnection.vb
+++ DatabaseConnection.vb
@@ -7,7 +7,7 @@ Public Class DatabaseConnection
             'Data Source=DESKTOP-8S284IC;Initial Catalog=aviationprojectDB;Integrated Security=True
             'Data Source=DESKTOP-KHI8921;Initial Catalog=aviationProjectDB;Integrated Security=True
 
-            Return "Data Source=DESKTOP-8S284IC;Initial Catalog=aviationprojectDB;Integrated Security=True"
+            Return "Data Source=DESKTOP-KHI8921;Initial Catalog=aviationProjectDB;Integrated Security=True"
         End Get
     End Property
 
